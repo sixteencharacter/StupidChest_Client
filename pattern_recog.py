0145846d6c94f8e162d5c9cd19b847fb45947822
sixteencharacter <66322506121@student.chula.ac.th>
2026-02-23 15:05:33 +0700

    update

--- pattern_recog.py
+++ pattern_recog.py
@@ -23,7 +23,7 @@ def calc_sliding_window(a,b) :
     min_mse = 1e6
     if a.size > 0 and b.size > 0 :
         for i in range(a.size - b.size) :
-            curr_mse = np.pow(a[i:i+b.size] - b,2) / b.size
+            curr_mse = np.pow(a[i:i+b.size] - b,2).sum() / b.size
             min_mse = min(min_mse,curr_mse)
     return min_mse
 
